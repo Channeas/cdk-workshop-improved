bd3fdce36f9bc2ad3d19316c9651c6fca8091319
Johannes <johannes.idarsson@gmail.com>
2022-07-16 14:06:50 +0200

    Renamed stack

--- bin/sampleApp.ts
+++ bin/sampleApp.ts
@@ -3,4 +3,4 @@ import * as cdk from "aws-cdk-lib";
 import { SampleAppStack } from "../lib/SampleAppStack";
 
 const app = new cdk.App();
-new SampleAppStack(app, "SampleAppStack");
+new SampleAppStack(app, "SampleAppStackWithImprovedHitCounter");
