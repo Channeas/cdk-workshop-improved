5b64da075cf3dc7a884449ed7c2f33e269849d4b
Johannes <johannes.idarsson@gmail.com>
2022-07-16 14:07:57 +0200

    Wrote an improved version of the hit counter (using SQS)

--- lambda/hitCounter.js
+++ lambda/hitCounter.js
@@ -1,36 +1,24 @@
-const { DynamoDB, Lambda } = require("aws-sdk");
+const { DynamoDB } = require("aws-sdk");
 
 exports.handler = async function (event) {
     console.log("Request:", JSON.stringify(event, undefined, 2));
 
     // Create AWS SDK clients
     const dynamoDB = new DynamoDB();
-    const lambda = new Lambda();
 
     // Update database entry for the path with hits++
-    await dynamoDB
-        .updateItem({
-            TableName: process.env.HITS_TABLE_NAME,
-            Key: { path: { S: event.path } },
-            UpdateExpression: "ADD hits :incr",
-            ExpressionAttributeValues: { ":incr": { N: "1" } },
-        })
-        .promise();
+    for (const sqsRecord of event.Records) {
+        const recordBody = JSON.parse(sqsRecord.body);
+        await dynamoDB
+            .updateItem({
+                TableName: process.env.HITS_TABLE_NAME,
+                Key: { path: { S: recordBody.path } },
+                UpdateExpression: "ADD hits :incr",
+                ExpressionAttributeValues: { ":incr": { N: "1" } },
+            })
+            .promise();
+    }
 
-    console.log("Hit recorded");
-
-    // Call the actual Lambda function and capture the response
-    // This seems like a stupid way to do it, now we will have 2x Lambda usage as this function will wait for the other to finish
-    // A much better way would be to fire off an event that triggers the hitCounter lambda
-    // Besides, if the hitCounter were to fail for any reason, the actual API call would also fail. The API should NOT depend on the hitCounter working properly
-    const response = await lambda
-        .invoke({
-            FunctionName: process.env.DOWNSTREAM_FUNCTION_NAME,
-            Payload: JSON.stringify(event),
-        })
-        .promise();
-
-    console.log("Downstream response:", JSON.stringify(response, undefined, 2));
-
-    return JSON.parse(response.Payload);
+    const moreThanOneHit = event.Records.length > 1;
+    console.log(`Hit${moreThanOneHit ? "s" : ""} recorded`);
 };
--- lib/HitCounter.ts
+++ lib/HitCounter.ts
@@ -1,15 +1,15 @@
-import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as cdk from "aws-cdk-lib";
 import * as dynamoDB from "aws-cdk-lib/aws-dynamodb";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as sqs from "aws-cdk-lib/aws-sqs";
 import { Construct } from "constructs";
-
-export interface HitCounterProps {
-    downstreamLambda: lambda.IFunction;
-}
+import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
 
 export class HitCounter extends Construct {
-    public readonly handler: lambda.Function;
+    public readonly hitQueue: sqs.Queue;
+    private processingLambda: lambda.Function;
 
-    constructor(scope: Construct, id: string, props: HitCounterProps) {
+    constructor(scope: Construct, id: string) {
         super(scope, id);
 
         const table = new dynamoDB.Table(this, "Hits", {
@@ -17,17 +17,22 @@ export class HitCounter extends Construct {
             billingMode: dynamoDB.BillingMode.PAY_PER_REQUEST,
         });
 
-        this.handler = new lambda.Function(this, "HitCounterHandler", {
+        this.hitQueue = new sqs.Queue(this, "HitQueue", {
+            visibilityTimeout: cdk.Duration.seconds(90),
+        });
+
+        this.processingLambda = new lambda.Function(this, "HitCounterHandler", {
             runtime: lambda.Runtime.NODEJS_16_X,
             handler: "hitCounter.handler",
             code: lambda.Code.fromAsset("lambda"),
             environment: {
-                DOWNSTREAM_FUNCTION_NAME: props.downstreamLambda.functionName,
                 HITS_TABLE_NAME: table.tableName,
             },
         });
 
-        table.grantReadWriteData(this.handler);
-        props.downstreamLambda.grantInvoke(this.handler);
+        const eventSource = new SqsEventSource(this.hitQueue);
+        this.processingLambda.addEventSource(eventSource);
+
+        table.grantReadWriteData(this.processingLambda);
     }
 }
