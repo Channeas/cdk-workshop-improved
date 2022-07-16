bd9d5ccd98c5c57b5815dffae7045405d8d41d4f
Johannes <johannes.idarsson@gmail.com>
2022-07-16 14:10:26 +0200

    Updated main stack to use the new hit counter

--- lib/SampleAppStack.ts
+++ lib/SampleAppStack.ts
@@ -1,26 +1,28 @@
-import { Stack, StackProps } from "aws-cdk-lib";
-import { Construct } from "constructs";
-import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apiGateway from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
 import { HitCounter } from "./hitCounter";
+import { Stack, StackProps } from "aws-cdk-lib";
 
 export class SampleAppStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        // Define a Lambda
-        const hello = new lambda.Function(this, "HelloHandler", {
+        const hitCounter = new HitCounter(this, "HitCounter");
+
+        const helloLambda = new lambda.Function(this, "HelloHandler", {
             runtime: lambda.Runtime.NODEJS_16_X,
-            code: lambda.Code.fromAsset("lambda"), // Load code from the lambda directory
-            handler: "hello.handler", // File is "hello", function is exports["handler"]
+            code: lambda.Code.fromAsset("lambda"),
+            handler: "hello.handler",
+            environment: {
+                HITCOUNTER_QUEUE_URL: hitCounter.hitQueue.queueUrl,
+            },
         });
 
-        const helloWithCounter = new HitCounter(this, "HelloHitCounter", {
-            downstreamLambda: hello,
-        });
+        hitCounter.hitQueue.grantSendMessages(helloLambda);
 
         new apiGateway.LambdaRestApi(this, "Endpoint", {
-            handler: helloWithCounter.handler,
+            handler: helloLambda,
         });
     }
 }
